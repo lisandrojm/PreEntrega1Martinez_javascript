f84f8f18a0542b201815d9155c12902b06a80f6f
lisandrojm <lisandrojm@gmail.com>
2023-01-21 14:35:25 -0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hello")
+// Bienvenida
+alert("Bienvenido a Freelander Calculator. Una herramienta simple para que sepas cuanto cobrar como freelancer.")
+//Paso 1 de 3
+prompt("Hello")
